1b5f4da4998714db34fd845d858357d977450112
GABOIESFH <141193745+GABOIESFH@users.noreply.github.com>
2023-12-20 17:11:08 -0600

    Fixing error

--- components/Form.js
+++ components/Form.js
@@ -7,21 +7,21 @@ export default function Form() {
 <form>
   <div className="grid gap-6 mb-6 md:grid-cols-2">
       <div>
-          <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+          <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
           <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre" required></input>
       </div>
       <div>
-          <label for="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellido</label>
+          <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellido</label>
           <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Apellido" required></input>
       </div>
        
       <div>
-          <label for="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefono</label>
+          <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefono</label>
           <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="614-1-234567" pattern="[0-9]{3}-[0-9]{1}-[0-9]{6}" required></input>
       </div>
 
       <div className="mb-6">
-        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
         <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@" required></input>
     </div> 
   </div>
