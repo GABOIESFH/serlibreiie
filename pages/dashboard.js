85365bfe7bb015fafa3f15d7d77c517d5c8cae3f
GABOIESFH <141193745+GABOIESFH@users.noreply.github.com>
2023-12-25 23:36:33 -0600

    Modifying dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -105,30 +105,15 @@ export default function Main() {
       
       <div>
         <div>
-          <h1>Instituto Ser Libre De Inteligencia Emocional</h1>
-          <h3>Bienvenidos</h3>
-          <p>texto de prueba</p>
-          <p>texto de prueba</p>
-          <p>texto de prueba</p>
-          <p>texto de prueba</p>
-          <p>texto de prueba</p>
-          <p>texto de prueba</p>
-          <p>texto de prueba</p>
-          <p>texto de prueba</p>
+          <h1 className="text-center font-extrabold text-8xl py-10">Bienvenido Administrador</h1>
+          
         </div>
-        <div className="flex justify-center">
-          <h1>Nuestra Ubicacion</h1>
-          <br />
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1229.1654237940943!2d-106.06692977094428!3d28.635841577239855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea435455de5e15%3A0x2ff5ea5977ebce60!2sC.%20Mariano%20Jim%C3%A9nez%201304%2C%20Obrera%2C%2031350%20Chihuahua%2C%20Chih.!5e0!3m2!1ses-419!2smx!4v1693525556708!5m2!1ses-419!2smx"
-            className='width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"'
-          ></iframe>
-        </div>
+        
        
       </div>
 
       {/* Formulario Grid Dos columnas */}
-      <div className="">
+      <div className="bg-black">
      
           <div className="grid gap-6 mb-6 md:grid-cols-3">
             {/* Nombre */}
@@ -260,6 +245,10 @@ export default function Main() {
           </div>
         ))}
       </main>
+
+      <footer className="italic text-lg font-extrabold text-center">
+        Developed by Gabooo
+      </footer>
     </>
   );
 }
